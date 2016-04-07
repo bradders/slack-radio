ffa59a5d2abae5123244b6aa00cb2ac0423f0ea3
Matt Bradley <mdbrad@gmail.com>
2016-04-07 09:51:29 -0700

    10-16 remove token check

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -9,50 +9,45 @@ class CodeController extends Controller {
   public function index(Request $request) {
 
     $response = [];
+    $response["response_type"] = "in_channel";
 
-    if( $request->input("token") == env("SLACK_TOKEN") ) {
+    $code = strtolower($request->input("text", "10-4"));
 
-      $response["response_type"] = "in_channel";
+    if( $code == "random" ) {
 
-      $code = strtolower($request->input("text", "10-4"));
+      // Pick one at random
+      $object = Code::orderByRaw("RAND()")->first();
+      $response["text"] = "{$object->code}: {$object->definition}";
 
-      if( $code == "random" ) {
+    } else if( $code == "help" ) {
 
-        // Pick one at random
-        $object = Code::orderByRaw("RAND()")->first();
-        $response["text"] = "{$object->code}: {$object->definition}";
+      // List all codes
+      $response["response_type"] = "ephemeral";
+      $response["text"] = "How to use /radio";
+      $response["attachments"][]["text"] = "Type /radio 10-4\nView all codes at http://slackrad.io\nValid keyword commands: random, list, help";
 
-      } else if( $code == "help" ) {
+    } else if( $code == "list" ) {
 
-        // List all codes
-        $response["response_type"] = "ephemeral";
-        $response["text"] = "How to use /radio";
-        $response["attachments"][]["text"] = "Type /radio 10-4\nView all codes at http://slackrad.io\nValid keyword commands: random, list, help";
+      $all = Code::all();
+      $response["response_type"] = "ephemeral";
+      $response["text"] = "All accepted APCO 10 codes:";
 
-      } else if( $code == "list" ) {
+      $str = "";
+      foreach( $all as $code ) {
+        $str .= "{$code->code}: {$code->definition}\n";
+      }
+      $response["attachments"][]["text"] = $str;
 
-        $all = Code::all();
-        $response["response_type"] = "ephemeral";
-        $response["text"] = "All accepted APCO 10 codes:";
+    } else {
 
-        $str = "";
-        foreach( $all as $code ) {
-          $str .= "{$code->code}: {$code->definition}\n";
-        }
-        $response["attachments"][]["text"] = $str;
-
+      if( $object = Code::where("code", "=", $code)->first() ) {
+        // Code found
+        $object->increment("count");
+        $response["attachments"][]["text"] = "{$object->code}: {$object->definition}";
       } else {
-
-        if( $object = Code::where("code", "=", $code)->first() ) {
-          // Code found
-          $object->increment("count");
-          $response["attachments"][]["text"] = "{$object->code}: {$object->definition}";
-        } else {
-          // Code not found
-          $response["response_type"] = "ephemeral";
-          $response["text"] = "Code not found, try `/radio help` for options";
-        }
-
+        // Code not found
+        $response["response_type"] = "ephemeral";
+        $response["text"] = "Code not found, try `/radio help` for options";
       }
 
     }
