d2619519c918f734f6743a52b5f35b09cfa98315
Matt Bradley <mdbrad@gmail.com>
2016-04-07 23:01:40 -0700

    10-89 CSS up in this biznatch

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,18 +1,44 @@
 <html>
-<body>
-  <h1>slackrad.io</h1>
+<head>
 
-  <?php if( isset($codes) ) { ?>
-  <dl>
-  <?php foreach( $codes as $code ) { ?>
-    <dt><?php echo $code->code; ?></dt>
-    <dd><?php echo $code->definition; ?></dd>
-  <?php } ?>
-  </dl>
-  <?php } ?>
+  <title>Slack Radio</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="stylesheet" href="/css/main.css">
+
+</head>
+<body>
+
+  <main class="section">
+
+    <div class="wrapper">
+
+      <h1>slackrad.io</h1>
+
+      <?php if( isset($codes) ) { ?>
+      <dl class="codes">
+      <?php foreach( $codes as $code ) { ?>
+        <dt class="code__code"><?php echo $code->code; ?>:</dt>
+        <dd class="code__definition"><?php echo $code->definition; ?></dd>
+      <?php } ?>
+      </dl>
+      <?php } ?>
+
+    </div>
+    <!-- /.wrapper -->
+
+  </main>
+
+  <footer class="section">
+
+    <div class="wrapper">
+      <p class="copyright">&copy; Copyright Matt Bradley 2016</p>
+    </div>
+    <!-- /.wrapper -->
+
+  </footer>
 
   <?php if(app()->environment() != "local") { ?>
-
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -22,8 +48,6 @@
     ga('create', 'UA-715960-11', 'auto');
     ga('send', 'pageview');
   </script>
-
   <?php } ?>
-
 </body>
 </html>
